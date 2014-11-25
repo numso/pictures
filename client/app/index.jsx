b836f9237558e86c9dd6a98bae37db19366219f6
Dallin Osmun <dosmun@gmail.com>
2014-11-24 19:10:48 -0700

    added instructions

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -18,6 +18,23 @@ var App = React.createClass({
         </div>
 
         <div style={{ width: 800, display: 'inline-block', verticalAlign: 'top' }}>
+
+          <div>
+            <div className="header">Instructions (this is all WIP)</div>
+            <div className="container --data" style={{minHeight: 10}}>
+              <div style={{padding: 10 }}>
+                <p style={{padding: 5}}>The equations below accept valid Javascript.</p>
+                <p style={{padding: 5}}>Arrays and Scalars are in the same context.</p>
+                <p style={{padding: 5}}>You must reference other pieces of data by their ID.</p>
+                <p style={{padding: 5}}>Format: z_#1_#2</p>
+                <p style={{padding: 5, paddingLeft: 40}}>z = a or s (array,scalar)</p>
+                <p style={{padding: 5, paddingLeft: 40}}>#1 = image number (always 1)</p>
+                <p style={{padding: 5, paddingLeft: 40}}>#2 = variable number (from 1 on)</p>
+                <p style={{padding: 5}}>Examples: s_1_3 or a_1_1</p>
+              </div>
+            </div>
+          </div>
+
           <Data/>
 
           <div>
