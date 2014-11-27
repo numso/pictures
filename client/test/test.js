6b609665a9d12d02c294c6387e35eed393faff9c
Dallin Osmun <dosmun@gmail.com>
2014-11-26 22:16:09 -0700

    fixed test page

--- client/test/test.js
+++ client/test/test.js
@@ -1,3 +1,5 @@
+require('./test.css');
+
 var $ = document.getElementById.bind(document);
 
 var obj = {
--- scripts/live-reload.js
+++ scripts/live-reload.js
@@ -3,10 +3,10 @@ var livereload = require('gulp-livereload');
 
 livereload.listen();
 
-watch('./client/build/bundle.js', function (filename) {
+watch('./client/build', function (filename) {
   livereload.changed();
 });
 
-watch('./client/build/bundle.css', function (filename) {
-  livereload.changed();
-});
+// watch('./client/build/bundle.css', function (filename) {
+//   livereload.changed();
+// });
