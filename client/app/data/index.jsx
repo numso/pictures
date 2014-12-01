e7bd890d48ce383ae96adef0012a8b45371e9bf6
Dallin Osmun <dosmun@gmail.com>
2014-11-30 17:34:49 -0700

    started adding flexbox to main app

--- client/app/data/index.jsx
+++ client/app/data/index.jsx
@@ -3,6 +3,7 @@ var _ = require('lodash');
 
 var Scalar = require('./scalar');
 var Arrayy = require('./arrayy');
+var ContentEditable = require('./content-editable');
 
 var store = require('../pictures-store');
 
@@ -36,7 +37,7 @@ var Data = React.createClass({
     return state;
   },
 
-  getDefaultProps: function() {
+  getDefaultProps() {
     return {
       pictureID: 0
     };
@@ -115,19 +116,59 @@ var Data = React.createClass({
     }, 0);
     var indices = _.range(1, max + 1);
 
+    // --- START FLEXBOX STUFF -------------------------------------------------
+
+    var tags = [];
+    _.each(this.state.scalars, (item, i) => {
+      tags.push(<div><ContentEditable draggable="true" text={item.label} className="tag"/></div>);
+    });
+    tags.push(<div><span onClick={this.createNew.bind(this, 'scalars')} className="tag --create">+</span></div>);
+    tags.push(<div><span className="tag --create">column</span></div>);
+    _.each(this.state.arrays, (item, i) => {
+      tags.push(<div><ContentEditable draggable="true" text={item.label} className="tag"/></div>);
+    });
+    tags.push(<span onClick={this.createNew.bind(this, 'arrays')} className="tag --create">+</span>);
+
+    var scalarValues = _.map(this.state.scalars, (item) => {
+      return <div style={{minHeight: 22}}><ContentEditable text={item.evaluated}/></div>;
+    });
+    var arrayValues = _.map(this.state.arrays, (item) => {
+      return <div>
+            {getArr(item.evaluated).map((item) => {
+              return <div className="data__indice">{item}</div>
+            })}
+          </div>;
+    });
+
+    // --- END FLEXBOX STUFF -------------------------------------------------
+
     return (
       <div className="data">
-        <div className="header">Data</div>
+        <div className="header">Data (table)</div>
         <div className="container --data">
           <table>
-          <tbody>
-            {s}
-            <tr><td><span onClick={this.createNew.bind(this, 'scalars')} className="tag --create">+</span></td></tr>
-            <tr><td></td><td><div>{indices.map((i) => { return <div className="data__indice --header">{i}</div>; })}</div></td></tr>
-            {a}
-            <tr><td><span onClick={this.createNew.bind(this, 'arrays')} className="tag --create">+</span></td></tr>
+            <tbody>
+              {s}
+              <tr><td><span onClick={this.createNew.bind(this, 'scalars')} className="tag --create">+</span></td></tr>
+              <tr><td></td><td><div>{indices.map((i) => { return <div className="data__indice --header">{i}</div>; })}</div></td></tr>
+              {a}
+              <tr><td><span onClick={this.createNew.bind(this, 'arrays')} className="tag --create">+</span></td></tr>
             </tbody>
           </table>
+        </div>
+        <div className="header">Data (flexbox)</div>
+        <div className="container --data --flex">
+          <div>
+            {tags}
+          </div>
+          <div className="test-1">
+            {scalarValues}
+            <div style={{minHeight: 22}}></div>
+            <div className="arr-section">
+              <div>{indices.map((i) => { return <div className="data__indice --header">{i}</div>; })}</div>
+              {arrayValues}
+            </div>
+          </div>
         </div>
         <div className="header">Results</div>
         <div className="container --data">
