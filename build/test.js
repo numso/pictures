4c29e2f6919281f7c3c7cbb99dece1437d6efffa
Dallin Osmun <dosmun@gmail.com>
2014-12-13 15:33:39 -0700

    push release

--- build/test.js
+++ build/test.js
@@ -40,23 +40,21 @@
 /******/ 	return __webpack_require__(0);
 /******/ })
 /************************************************************************/
-/******/ ({
-
-/***/ 0:
+/******/ ([
+/* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	__webpack_require__(4);
+	__webpack_require__(3);
 
 	__webpack_require__(1);
 	__webpack_require__(2);
 
 
 /***/ },
-
-/***/ 1:
+/* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var $ = __webpack_require__(3);
+	var $ = __webpack_require__(5);
 
 	var obj = {
 	  tag: 'yayahoo',
@@ -139,11 +137,10 @@
 
 
 /***/ },
-
-/***/ 2:
+/* 2 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var $ = __webpack_require__(3);
+	var $ = __webpack_require__(5);
 
 	var originalX;
 	var originalNum = 5;
@@ -178,23 +175,13 @@
 
 
 /***/ },
-
-/***/ 3:
-/***/ function(module, exports, __webpack_require__) {
-
-	var $ = document.getElementById.bind(document);
-	module.exports = $;
-
-
-/***/ },
-
-/***/ 4:
+/* 3 */
 /***/ function(module, exports, __webpack_require__) {
 
 	// style-loader: Adds some css to the DOM by adding a <style> tag
 
 	// load the styles
-	var content = __webpack_require__(5);
+	var content = __webpack_require__(4);
 	if(typeof content === 'string') content = [[module.id, content, '']];
 	// add the styles to the DOM
 	var update = __webpack_require__(6)(content, {});
@@ -211,16 +198,22 @@
 	}
 
 /***/ },
-
-/***/ 5:
+/* 4 */
 /***/ function(module, exports, __webpack_require__) {
 
-	exports = module.exports = __webpack_require__(24)();
+	exports = module.exports = __webpack_require__(22)();
 	exports.push([module.id, "* {\n  font-family: sans-serif;\n}\n.tag {\n  background-color: #abbbd9;\n  border-radius: 15px;\n  color: #fff;\n  cursor: pointer;\n  padding: 2px 10px;\n  margin: 2px 0;\n  margin-right: 10px;\n  font-size: 12px;\n  letter-spacing: 2px;\n  display: inline-block;\n  min-width: 20px;\n  min-height: 12px;\n}\n#demo3 {\n  width: 700px;\n}\n#demo3 .container {\n  background: #eee;\n}\n#demo3 .container .row {\n  display: flex;\n}\n#demo3 .container .row .expr {\n  flex: 1;\n  white-space: nowrap;\n  overflow: auto;\n}\n#demo3 .container .row .expr::-webkit-scrollbar {\n  display: none;\n}\n#demo3 .container .row .expr input {\n  width: 100%;\n}\n#demo3 .container .row .expr:hover {\n  background-color: #ccc;\n  cursor: pointer;\n}\n#demo4 {\n  width: 700px;\n}\n#demo4 .container {\n  background: #eee;\n  display: flex;\n}\n#demo4 .container .expressions {\n  flex: 1;\n  overflow: auto;\n}\n#demo4 .container .expressions::-webkit-scrollbar {\n  display: none;\n}\n#demo4 .container .expr {\n  width: 100%;\n  height: 22px;\n  white-space: nowrap;\n}\n#demo4 .container .expr input {\n  width: 100%;\n}\n#demo4 .container .expr:hover {\n  background-color: #ccc;\n  cursor: pointer;\n}\n", ""]);
 
 /***/ },
+/* 5 */
+/***/ function(module, exports, __webpack_require__) {
 
-/***/ 6:
+	var $ = document.getElementById.bind(document);
+	module.exports = $;
+
+
+/***/ },
+/* 6 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/*
@@ -416,8 +409,22 @@
 
 
 /***/ },
-
-/***/ 24:
+/* 7 */,
+/* 8 */,
+/* 9 */,
+/* 10 */,
+/* 11 */,
+/* 12 */,
+/* 13 */,
+/* 14 */,
+/* 15 */,
+/* 16 */,
+/* 17 */,
+/* 18 */,
+/* 19 */,
+/* 20 */,
+/* 21 */,
+/* 22 */
 /***/ function(module, exports, __webpack_require__) {
 
 	module.exports = function() {
@@ -438,5 +445,4 @@
 	}
 
 /***/ }
-
-/******/ })
+/******/ ])
