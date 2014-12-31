6d74cacb9612aebaeec4855898625f9eaa9a915b
Dallin Osmun <dosmun@gmail.com>
2014-12-30 23:52:22 -0700

    added d3 to the project. gotta figure out how d3 works with react...

--- client/app/big-picture/index.jsx
+++ client/app/big-picture/index.jsx
@@ -1,10 +1,12 @@
 var React = require('react');
+var d3 = require('d3');
 
 var BigPicture = React.createClass({
 
   render() {
     return (
-      <div className="picture --big"/>
+      <div className="picture --big">
+      </div>
     );
   }
 
