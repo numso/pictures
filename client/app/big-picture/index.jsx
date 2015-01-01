4726030c0cdcf7c54baf4580394c919812701f55
Dallin Osmun <dosmun@gmail.com>
2014-12-31 23:26:37 -0700

    woo hoo. got something to draw in react

--- client/app/big-picture/index.jsx
+++ client/app/big-picture/index.jsx
@@ -3,9 +3,22 @@ var d3 = require('d3');
 
 var BigPicture = React.createClass({
 
+  componentDidMount() {
+    this.svg = d3.select('#picture-box').append('svg')
+      .attr('width', 500)
+      .attr('height', 500);
+
+    this.svg.append('circle')
+      .attr('class', 'real-circle')
+      .attr('cx', 150)
+      .attr('cy', 150)
+      .attr('r', 50)
+      .attr('fill', '#333');
+  },
+
   render() {
     return (
-      <div className="picture --big">
+      <div id="picture-box" className="picture --big">
       </div>
     );
   }
