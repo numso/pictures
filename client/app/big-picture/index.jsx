9208074f30bddd1aa31354d9f1a1f71a356e6363
Dallin Osmun <dosmun@gmail.com>
2015-01-11 08:59:25 -0700

    turned big picture into omniscient component with cursors

--- client/app/big-picture/index.jsx
+++ client/app/big-picture/index.jsx
@@ -1,10 +1,5 @@
+var {component} = require('omniscient-tools');
 var immutable = require('immutable');
-var React = require('react');
-
-var thiz;
-
-var blue = '#56a7e1';
-var grey = '#cacaca';
 
 var startx = null;
 var starty;
@@ -16,63 +11,60 @@ function mouseDown(e) {
   starty = e.nativeEvent.offsetY;
 }
 
-function mouseMove(e) {
-  var mode = thiz.props.mode.get('current');
-  if (startx === null) return;
-  var endx = e.nativeEvent.offsetX;
-  var endy = e.nativeEvent.offsetY;
-  if (mode === 'circle') {
-    var dist = distance(startx, starty, endx, endy);
-    setPreview('circle', { type: 'circle', x1: startx, y1: starty, r: dist });
-    thiz.props.bigPictureStuff.update('msg', () => `Draw circle around (${startx}, ${starty}), ${Math.floor(dist)} px in radius.`);
-  } else  if (mode === 'rect') {
-    setPreview('rect', { type: 'rect', x1: startx, y1: starty, x2: endx, y2: endy });
-    thiz.props.bigPictureStuff.update('msg', () => `Draw rect from (${startx}, ${starty}), ${endx - startx} px horizontally, ${endy - starty} px vertically.`);
-  } else if (mode === 'line') {
-    setPreview('line', { type: 'line', x1: startx, y1: starty, x2: endx, y2: endy });
-    thiz.props.bigPictureStuff.update('msg', () => `Draw line from (${startx}, ${starty}), ${endx - startx} px horizontally, ${endy - starty} px vertically.`);
-  }
+function mouseMove(mode, bigPictureStuff) {
+  return function (e) {
+    var _mode = mode.get('current');
+    if (startx === null) return;
+    var endx = e.nativeEvent.offsetX;
+    var endy = e.nativeEvent.offsetY;
+    if (_mode === 'circle') {
+      var dist = distance(startx, starty, endx, endy);
+      setPreview(bigPictureStuff, 'circle', { type: 'circle', x1: startx, y1: starty, r: dist });
+      bigPictureStuff.update('msg', () => `Draw circle around (${startx}, ${starty}), ${Math.floor(dist)} px in radius.`);
+    } else  if (_mode === 'rect') {
+      setPreview(bigPictureStuff, 'rect', { type: 'rect', x1: startx, y1: starty, x2: endx, y2: endy });
+      bigPictureStuff.update('msg', () => `Draw rect from (${startx}, ${starty}), ${endx - startx} px horizontally, ${endy - starty} px vertically.`);
+    } else if (_mode === 'line') {
+      setPreview(bigPictureStuff, 'line', { type: 'line', x1: startx, y1: starty, x2: endx, y2: endy });
+      bigPictureStuff.update('msg', () => `Draw line from (${startx}, ${starty}), ${endx - startx} px horizontally, ${endy - starty} px vertically.`);
+    }
+  };
 }
 
-function mouseUp(e) {
-  var mode = thiz.props.mode.get('current');
-  var endx = e.nativeEvent.offsetX;
-  var endy = e.nativeEvent.offsetY;
-  if (mode === 'circle') {
-    var dist = distance(startx, starty, endx, endy);
-    addStep({ type: 'circle', x1: startx, y1: starty, r: dist });
-  } else if (mode === 'rect') {
-    addStep({ type: 'rect', x1: startx, y1: starty, x2: endx, y2: endy });
-  } else if (mode === 'line') {
-    addStep({ type: 'line', x1: startx, y1: starty, x2: endx, y2: endy });
-  } else if (mode === 'text') {
-    thiz.props.bigPictureStuff.update('msg', () => `Draw text at (${endx}, ${endy})`);
-    addStep({ type: 'text', x1: endx, y1: endy });
-  }
-  startx = null;
+function mouseUp(mode, bigPictureStuff, steps) {
+  return function (e) {
+    var _mode = mode.get('current');
+    var endx = e.nativeEvent.offsetX;
+    var endy = e.nativeEvent.offsetY;
+    if (_mode === 'circle') {
+      var dist = distance(startx, starty, endx, endy);
+      addStep(steps, { type: 'circle', x1: startx, y1: starty, r: dist });
+    } else if (_mode === 'rect') {
+      addStep(steps, { type: 'rect', x1: startx, y1: starty, x2: endx, y2: endy });
+    } else if (_mode === 'line') {
+      addStep(steps, { type: 'line', x1: startx, y1: starty, x2: endx, y2: endy });
+    } else if (_mode === 'text') {
+      bigPictureStuff.update('msg', () => `Draw text at (${endx}, ${endy})`);
+      addStep(steps, { type: 'text', x1: endx, y1: endy });
+    }
+    startx = null;
+  };
 }
 
 // --- helper functions --------------------------------------------------------
-
-function midpoint(x, y, x2, y2) {
-  return {
-    x: (x + x2) / 2,
-    y: (y + y2) / 2
-  };
-}
 
 function distance(x, y, x2, y2) {
   return Math.sqrt(Math.pow(x2 - x, 2) + Math.pow(y2 - y, 2));
 }
 
-function addStep(step) {
-  thiz.props.steps.update((oldSteps) => {
+function addStep(steps, step) {
+  steps.update((oldSteps) => {
     return oldSteps.push(immutable.fromJS(step));
   });
 }
 
-function setPreview(key, preview) {
-  thiz.props.bigPictureStuff.get('previews').update(key, () => immutable.fromJS(preview));
+function setPreview(bigPictureStuff, key, preview) {
+  bigPictureStuff.get('previews').update(key, () => immutable.fromJS(preview));
 }
 
 function getSVGParts(steps) {
@@ -88,36 +80,27 @@ function getSVGParts(steps) {
         svgParts.push(<rect x={x} y={y} width={w} height={h} fill="#cacaca"/>);
         break;
       case 'line': svgParts.push(<line x1={s.get('x1')} y1={s.get('y1')} x2={s.get('x2')} y2={s.get('y2')} stroke="#cacaca"/>); break;
-      case 'text': svgParts.push(<text x={s.get('x1')} y={s.get('y1')} fill="#cacaca" fontSize="4">Hi!</text>); break;
+      case 'text': svgParts.push(<text x={s.get('x1')} y={s.get('y1')} fill="#cacaca">Hi!</text>); break;
     }
   }).toJS();
   return svgParts;
 }
 
-var BigPicture = React.createClass({
+module.exports = component(function ({mode, steps, bigPictureStuff}) {
 
-  componentDidMount() {
-    thiz = this;
-  },
+  var svgParts = getSVGParts(steps);
+  var previewParts = getSVGParts(bigPictureStuff.get('previews'));
 
-  render() {
-    thiz = this;
-    var svgParts = getSVGParts(thiz.props.steps);
-    var previewParts = getSVGParts(thiz.props.bigPictureStuff.get('previews'));
-
-    return (
-      <div>
-        <div style={{textAlign: 'center', height: 18}}>{this.props.bigPictureStuff.get('msg')}</div>
-        <div id="picture-box" className="picture --big">
-          <svg width={720} height={900} onMouseDown={mouseDown} onMouseUp={mouseUp} onMouseMove={mouseMove}>
-            {svgParts}
-            {previewParts}
-          </svg>
-        </div>
+  return (
+    <div>
+      <div style={{textAlign: 'center', height: 18}}>{bigPictureStuff.get('msg')}</div>
+      <div id="picture-box" className="picture --big">
+        <svg width={720} height={900} onMouseDown={mouseDown} onMouseUp={mouseUp(mode, bigPictureStuff, steps)} onMouseMove={mouseMove(mode, bigPictureStuff)}>
+          {svgParts}
+          {previewParts}
+        </svg>
       </div>
-    );
-  }
+    </div>
+  );
 
 });
-
-module.exports = BigPicture;
